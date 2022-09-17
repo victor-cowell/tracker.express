cf8c64c10e53a781ead750ed5603efa727df4890
k1onix <vkolesnikov@bestmood.tech>
2022-09-17 12:35:33 +0300

    feat: add sending template in response

--- src/template-service/template.ts
+++ src/template-service/template.ts
@@ -1,11 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
 
 const app: Express = express();
 const port = parseInt(process.env.TEMPLATE_SERVICE_PORT!);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Template Server is running');
-});
+const sendTemplate: (req: Request, res: Response) => void = (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+};
+
+app.get('/', sendTemplate);
+//TODO recheck how we can handle it in single app.get
+app.get('/1.html', sendTemplate);
+app.get('/2.html', sendTemplate);
 
 export const templateService = {
   name: 'Template Service',
